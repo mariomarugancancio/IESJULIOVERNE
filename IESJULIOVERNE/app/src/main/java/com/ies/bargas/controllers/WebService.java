7133262fc11e9cc036dd2672b01a88498df65722
Nataliabd82 <nataliabd8282@gmail.com>
2024-05-14 16:06:30 +0200

    FragmentSemana con webService

--- IESJULIOVERNE/app/src/main/java/com/ies/bargas/controllers/WebService.java
+++ IESJULIOVERNE/app/src/main/java/com/ies/bargas/controllers/WebService.java
@@ -2,14 +2,15 @@ package com.ies.bargas.controllers;
 
 public class WebService {
     //public static final String RAIZ = "https://iesbargas.es/android/"; //Hosting
-    public static final String RAIZ = "http://192.168.150.154/IESJULIOVERNE/android/"; //tener en cuenta en vez de poner localhost poner la ip
+    public static final String RAIZ = "http://172.21.0.100/IESJULIOVERNE/android/"; //tener en cuenta en vez de poner localhost poner la ip
     public static final String LOGIN = "usuarios/login.php";
     public static final String SIGNUP = "usuarios/signup.php";
     public static final String Departamentos = "usuarios/findAllDepartamentos.php";
     public static final String Cursos = "usuarios/findAllCursos.php";
     public static final String Recover = "usuarios/recover.php";
     public static final String Modify = "usuarios/modify.php";
-   //Partes
+
+    //Partes
     public static final String Asignaturas="partes/findAsignaturas.php";
     public static final String Incidencias="partes/findAllIncidencias.php";
     public static final String Alumnos="partes/findAlumnos.php";
@@ -17,16 +18,14 @@ public class WebService {
     public static final String InsertExpulsiones="partes/insertExpulsiones.php";
     public static final String SelectPartes ="partes/selectPartes.php";
 
-
-
-//Guardias
-public static final String AddShifts = "guardias/addShifts.php";
+    //Guardias
+    public static final String AddShifts = "guardias/addShifts.php";
     public static final String Periodos = "guardias/periodos.php";
     public static final String Usuarios = "guardias/usuarios.php";
     public static final String Edit = "guardias/editShift.php";
     public static final String Delete = "guardias/deleteShift.php";
     public static final String ObtenerGuardias = "guardias/getShifts.php";
+    public static final String ObtenerGuardiasSemana = "guardias/getShiftWeek.php";
+
 
 }
-
-
