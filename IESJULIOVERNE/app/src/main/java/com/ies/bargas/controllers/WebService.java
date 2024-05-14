fd594f5f9d1fe632df9b6f38b5472e4523592b05
= <Zhenyu12wuyin@gmail.com>
2024-05-14 16:05:53 +0200

    Update WebService.java

--- IESJULIOVERNE/app/src/main/java/com/ies/bargas/controllers/WebService.java
+++ IESJULIOVERNE/app/src/main/java/com/ies/bargas/controllers/WebService.java
@@ -2,13 +2,16 @@ package com.ies.bargas.controllers;
 
 public class WebService {
     //public static final String RAIZ = "https://iesbargas.es/android/"; //Hosting
-    public static final String RAIZ = "http://192.168.150.154/IESJULIOVERNE/android/"; //tener en cuenta en vez de poner localhost poner la ip
+    //public static final String RAIZ = "http://192.168.150.154/IESJULIOVERNE/android/"; //tener en cuenta en vez de poner localhost poner la ip
+    public static final String RAIZ = "http://192.168.1.13/IESJULIOVERNE/android/"; //IPPP
     public static final String LOGIN = "usuarios/login.php";
     public static final String SIGNUP = "usuarios/signup.php";
     public static final String Departamentos = "usuarios/findAllDepartamentos.php";
     public static final String Cursos = "usuarios/findAllCursos.php";
     public static final String Recover = "usuarios/recover.php";
     public static final String Modify = "usuarios/modify.php";
+
+
    //Partes
     public static final String Asignaturas="partes/findAsignaturas.php";
     public static final String Incidencias="partes/findAllIncidencias.php";
@@ -16,11 +19,15 @@ public class WebService {
     public static final String InsertParts="partes/insertParts.php";
     public static final String InsertExpulsiones="partes/insertExpulsiones.php";
     public static final String SelectPartes ="partes/selectPartes.php";
+    public static final String InsertAlumno="partes/insertAlumno.php";
+    public static final String SelectAlumnos="partes/selectAlumnos.php";
+    public static final String ModificarAlumno="partes/modifyAlumno.php";
+    public static final String DeleteAlumno="partes/deleteAlumno.php";
 
 
 
-//Guardias
-public static final String AddShifts = "guardias/addShifts.php";
+    //Guardias
+    public static final String AddShifts = "guardias/addShifts.php";
     public static final String Periodos = "guardias/periodos.php";
     public static final String Usuarios = "guardias/usuarios.php";
     public static final String Edit = "guardias/editShift.php";
