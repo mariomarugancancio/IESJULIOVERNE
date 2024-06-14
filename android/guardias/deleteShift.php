53f427200b9f979bc5cb53753682d50eca219a08
Nataliabd82 <nataliabd8282@gmail.com>
2024-06-14 12:12:28 +0200

    Cambios

--- android/guardias/deleteShift.php
+++ android/guardias/deleteShift.php
@@ -1,18 +1,41 @@
 <?php
 include '../conexion.php';
 
-// Obtener los valores de los atributos de la clase Partes a través de la URL
-$cod_guardias = $_GET['cod_guardias'];
-// Consulta SQL con marcadores de posición (?)
-$sql = "DELETE FROM guardias WHERE cod_guardias = ?";
-// Preparar la consulta
-$stmt = $db->prepare($sql);
-// Ejecutar la consulta con los valores proporcionados
-$stmt->execute(array($cod_guardias));
+// Obtener los valores de los atributos de la clase Guardias a través de la URL
+$cod_parte = $_GET['cod_guardias'];
+
+$sql = "SELECT * FROM Guardias WHERE cod_guardias=?;";
+$consulta = $db->prepare($sql);
+$consulta->execute(array($cod_parte));
+
+if ($consulta->rowCount() == 1){
+
+    // Consulta SQL con marcadores de posición (?)
+    $sql = "DELETE FROM Guardias WHERE cod_guardias=?;";
+
+    // Preparar la consulta
+    $consulta = $db->prepare($sql);
+
+    // Ejecutar la consulta con los valores proporcionados
+    $consulta->execute(array($cod_guardias));
+
+    $sql = "SELECT * FROM Guardias WHERE cod_guardias=?;";
+    $consulta = $db->prepare($sql);
+    $consulta->execute(array($cod_guardias));
+
+    if ($consulta->rowCount() == 1){
+        $respuesta=2;
+    } else {
+        $respuesta=1;
+    }
+
+} else {
+    $respuesta=0;
+}
   
 header ('Content-Type: application/json');
 
-echo "Guardia eliminada correctamente";
+echo $respuesta;
 
 ?>
 
